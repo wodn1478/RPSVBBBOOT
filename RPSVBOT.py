9d188bac7aa198c8307f303c6212ce36d48ff6ba
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2019-03-21 00:03:07 +0900

    Update RPSVBOT.py

--- RPSVBOT.py
+++ RPSVBOT.py
@@ -31,6 +31,7 @@ async def on_message(message):
         server = message.server
         author = message.author.id
         name = message.author.name
+        name2 = name.lower()
         everyone = discord.PermissionOverwrite(read_messages=False, send_messages=False, create_instant_invite=False,
                                                        manage_channel=False, manage_permissions=False, manage_webhooks=False,
                                                        send_TTS_messages=False, manage_messages=False, embed_links=False,
@@ -39,12 +40,12 @@ async def on_message(message):
         Member = discord.PermissionOverwrite(read_messages=True, send_messages=True, create_instant_invite=False,read_message_history=True,
                                                      manage_channel=False, manage_permissions=False, manage_webhooks=False)
         member_perms = [(mentioned, Member) for mentioned in message.mentions]
-        await bot.create_channel(server, name, (discord.utils.get(message.server.roles, name="@everyone"), everyone),
+        await bot.create_channel(server, name2, (discord.utils.get(message.server.roles, name="@everyone"), everyone),
                                             (discord.utils.get(message.server.members, name=name), Member),
                                          *member_perms, type=discord.ChannelType.text)
         
         
-        await bot.send_message(discord.utils.get(message.server.channels, name=name), "@everyone\n"+ "<@"+id+">님의 상담을 시작합니다.")
+        await bot.send_message(discord.utils.get(message.server.channels, name=name2), "@everyone\n"+ "<@"+id+">님의 상담을 시작합니다.")
         
         
     if message.content.startswith("공지띄우기1"):
