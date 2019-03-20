5f449eb45b5f1c7a88b4847dcee066372c277f2d
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2019-03-20 18:42:20 +0900

    Update RPSVBOT.py

--- RPSVBOT.py
+++ RPSVBOT.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import requests
-import re
+
 from bs4 import BeautifulSoup as bs
 client = discord.Client()
 bot = commands.Bot(command_prefix='T')
